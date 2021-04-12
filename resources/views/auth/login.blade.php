d8db0a7b4586a5e679b6e2ce772f22d1dc5686e1
unknown <andrescs32>
2021-04-12 15:18:50 -0500

    vistas login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,44 +1,55 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <link href="css/form.css" rel="stylesheet"/>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <title>loginUsuarios</title>
-        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
-    </head>
-    <body>
-        
-        <div class="container col-lg-3">
-            <form action="{{route('login')}}" method="post">
+<head>
+    <link href="css/form.css" rel="stylesheet"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>loginUsuarios</title>
+	
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<img class="wave" src="img/wave.svg">
+	<div class="container">
+		<div class="img">
+			<img src="img/bg.png">
+		</div>
+		<div class="login-content">
+			<form action="{{route('login')}}" method="post">
                 @csrf
-                <div class="form-group text-center">
-                    <img src="imgform/favicon.png" height="100" width="110"/>
-                    <p><strong>Bienvenido al Sistema Pet Urgency</strong></p>
-                </div>
-                <div class="form-group">
-                <label>Email:</label>
-                <input type="email" name="email" placeholder="ejemplo@gmail.com" class="form-control" required>
-                
-                </div>
-                <div class="form-group">
-                <label>Contraseña:</label>
-                <input type="password" name="password" placeholder="*******" class="form-control" required>
-                </div>
-                <center>
-                <input class="btn btn-primary bnt-block" type="submit" name="accion" value="Iniciar sesion" >
-                <a href="{{route('register')}}" class="btn btn-primary">Registrarse</a>
-                <BR></BR>
-                <a href="{{route('index')}}" class="btn btn-primary">Regresar al inicio</a>
-                
-                
-                </center>
-
+				<img src="img/favicon.png">
+				<br>
+				<h3 class="title">Bienvenido al Sistema Pet Urgency</h3>
+				<br>
+           		<div class="input-div one">
+           		   <div class="i">
+					<i class="far fa-envelope"></i>
+           		   </div>
+           		   <div class="div">
+           		   		<h5>Correo Electronico</h5>
+           		   		<input type="email" name="email"  class="input" required>
+           		   </div>
+           		</div>
+           		<div class="input-div pass">
+           		   <div class="i"> 
+           		    	<i class="fas fa-lock"></i>
+           		   </div>
+           		   <div class="div">
+           		    	<h5>Contraseña</h5>
+           		    	<input type="password" name="password" class="input" required>
+            	   </div>
+            	</div>
+				
+            	<a href="#">¿Has olvidado tu contraseña? </a>
+            	<input class="btn btn-primary bnt-block" type="submit" name="accion" value="Iniciar Sesión">
+                <a href="{{route('index')}}" class="btn btn-primary">Regresar</a>  
             </form>
-
         </div>
-
+    </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    </body>
+        <script type="text/javascript" src="js/form.js"></script>
+</body>
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,55 +1,85 @@
+<!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Animated Login Form</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
         <link href="css/form.css" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <title>RegistrarUsuarios</title>
-        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+        <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
-    
     <body>
-        
-        <div class="container col-lg-3">
-            <form action="{{route('register')}}" method="POST">
-                 @csrf
-                <div class="form-group text-center">
-                    <img src="imgform/favicon.png" height="100" width="110"/>
-                    <p><strong>Bienvenido al Sistema Pet Urgency</strong></p>
-                </div>
-                <div class="form-group">
-                <label>Nombres:</label>
-                <input class="form-control" type="text" name="name" placeholder="Ingrese su nombre completo" title="Es obligatorio que ingreses tu nombre" required>
-                </div>
-                <div class="form-group">
-                <label>Email:</label>
-                <input type="email" name="email" placeholder="ejemplo@gmail.com" class="form-control" title="Es obligatorio que ingreses tu e-mail"  required>
-                </div>
-                <div class="form-group">
-                    <label>Telefono:</label>
-                    <input type="text" name="phone" placeholder="3146190528" class="form-control" title="Es obligatorio que ingreses tu numero celular"  required>
-                </div>
-                <div class="form-group">
-                    <label>genero:</label>
-                    <input type="text" name="gender" placeholder="" class="form-control" title="Es obligatorio que ingreses tu genero"  required>
+        <img class="wave" src="img/wave.svg">
+        <div class="container">
+            <div class="img">
+                <img src="img/bg.png">
+            </div>
+            <div class="login-content">
+                <form action="{{route('register')}}" method="POST">
+                    @csrf
+                    <img src="img/favicon.png">
+                    <br>
+                    <h3 class="title">Bienvenido al Sistema Pet Urgency</h3>
+                    <br>
+                    <div class="input-div one">
+                    <div class="i">
+                            <i class="fas fa-user"></i>
                     </div>
-                <div class="form-group">
-                <label>Contraseña:</label>
-                <input type="password"pattern=".{8,}"name="password" placeholder="********" class="form-control" title="Es obligatorio que ingreses tu Contraseña (Maximo 8 caracteres)" required>
-                </div>
-                <center>
-                <input class="btn btn-primary bnt-block" type="submit" name="accion" value="Registrar">
-                <a href="{{route('login')}}" class="btn btn-primary">Iniciar sesion</a>
-                <BR></BR>
-                <a href="{{route('index')}}" class="btn btn-primary">Regresar al inicio</a>
-                
-                </center>
-
-            </form>
-            
+                    <div class="div">
+                            <h5>Nombres</h5>
+                            <input type="text" class="input" name="name"title="Es obligatorio que ingreses tu nombre" required>
+                    </div>
+                    </div>
+                    <div class="input-div pass">
+                    <div class="i"> 
+                        <i class="far fa-envelope"></i>
+                    </div>
+                    <div class="div">
+                            <h5>Correo Electronico</h5>
+                            <input  type="email" name="email" class="input" title="Es obligatorio que ingreses tu e-mail"  required> 
+                    </div>
+                    </div>
+                    <div class="input-div pass">
+                        <div class="i"> 
+                            <i class="fas fa-phone-square-alt"></i>
+                        </div>
+                        <div class="div">
+                            <h5>Telefono</h5>
+                            <input type="text" name="phone" placeholder="" class="input" title="Es obligatorio que ingreses tu numero celular"  required>
+                    </div>
+                    </div>
+                    <div class="input-div pass">
+                        <div class="i"> 
+                            <i class="fas fa-venus-mars"></i>
+                        </div>
+                        <div class="div">
+                            <h5>Genero</h5>
+                            <input type="text" name="gender" class="input" title="Es obligatorio que ingreses tu genero"  required>
+                    </div>
+                    </div>
+                    <div class="input-div pass">
+                        <div class="i"> 
+                            <i class="fas fa-lock"></i>
+                        </div>
+                        <div class="div">
+                            <h5>Contraseña</h5>
+                            <input type="password"pattern=".{8,}"name="password" placeholder="********" class="input" title="Es obligatorio que ingreses tu Contraseña (Maximo 8 caracteres)" required>
+                    </div>
+                    </div>
+                    
+                    
+                    <a href="#">¿Has olvidado tu contraseña? </a>
+                    <input type="submit" class="btn btn-primary bnt-block"  name="accion" value="Registrarse">
+                    <a href="{{route('index')}}" class="btn btn-primary">Regresar</a>
+                    
+                    
+                </form>
+            </div>
         </div>
-
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+            <script type="text/javascript" src="js/form.js"></script>
     </body>
 </html>
+
